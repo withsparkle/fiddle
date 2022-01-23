02b1e428a7943d98d86c6d143e20131bc427745e
Kamil Samigullin <kamil@samigullin.info>
2022-01-23 08:37:27 +0300

    dev: integrate ferret

---
+++ bin/lib/tools/ferret.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# shellcheck source=../git/git.bash # @root
+
+_ferret=$(which ferret || true)
+
+# Example: run ferret exec fetch.fql | jq
+ferret() {
+  [ -z "${_ferret}" ] && @fatal Please setup environment first
+
+  pushd "$(@root)/tests/requests" >/dev/null || exit 1
+  trap 'popd >/dev/null' ERR
+
+  "${_ferret}" "$@"
+
+  popd >/dev/null || exit 1
+}
